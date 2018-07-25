0495ac21aa5e36e7b28711a4b44ff03f070958f5
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 16:11:29 +0200

    exo4

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercic 4</title>
+</head>
+<body>
+  <p>
+    <?php
+    //variable de type string
+    $name = 'James Bond';
+    //variable de type int
+    $id = 007;
+    //variable de type float
+    $ratio = 9.8;
+    //variable de type boolean
+    $agent = false;
+
+    echo $name;
+    echo $id;
+    echo $ratio;
+    echo $agent;
+    ?>
+  </p>
+</body>
+</html>
